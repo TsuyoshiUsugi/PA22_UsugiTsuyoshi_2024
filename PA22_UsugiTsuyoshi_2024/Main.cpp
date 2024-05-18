5f0449876485b617711e6c1ef1158626a035c0bf
TsuyoshiUsugi <qesulive@gmail.com>
2024-05-18 17:21:35 +0900

    [update] 課題終了

--- PA22_UsugiTsuyoshi_2024/Main.cpp
+++ PA22_UsugiTsuyoshi_2024/Main.cpp
@@ -1,105 +1,144 @@
 ﻿# include <Siv3D.hpp> // Siv3D v0.6.13
 
+//titleの定数
+constexpr Size TITLE_SIZE = { 800, 600 };
+constexpr Vec2 TITLE_POS = { 400, 300 };
+
+//インゲームの定数
+constexpr int RADIUS = 40;
+constexpr float BALLSPEED = 500.0f;
+constexpr Vec2 BALLSIZE = { 40, 20 };
+constexpr Vec2 BLOCKSIZE = { 40, 20 };
+
+constexpr int XCOUNT = 20;
+constexpr int YCOUNT = 5;
+constexpr int MAX = XCOUNT * YCOUNT;
+constexpr Size BRICKSIZE = { 40, 20 };
+constexpr int BALLDUR = 5;
+
+
 void Main()
 {
-	// 背景の色を設定する | Set the background color
-	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
+title:
+#pragma region タイトル
+	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+	Font font(60);
+	while (System::Update())
+	{
+		font(U"ブロック崩し").drawAt(Scene::Center(), Palette::Skyblue);
 
-	// 画像ファイルからテクスチャを作成する | Create a texture from an image file
-	const Texture texture{ U"example/windmill.png" };
+		if (SimpleGUI::ButtonAt(U"スタート", TITLE_POS + Vec2{ 0, 100 }))
+		{
+			break;
+		}
+	}
+#pragma endregion
 
-	// 絵文字からテクスチャを作成する | Create a texture from an emoji
-	const Texture emoji{ U"🦖"_emoji };
+#pragma region インゲーム
+	Vec2 ballVelocity{ 0, -BALLSPEED };
+	Circle ball{ 400, 400, 8 };
+	Rect bricks[MAX];
+	int Score = 0;
+	bool isGameOver = false;
 
-	// 太文字のフォントを作成する | Create a bold font with MSDF method
-	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
-
-	// テキストに含まれる絵文字のためのフォントを作成し、font に追加する | Create a font for emojis in text and add it to font as a fallback
-	const Font emojiFont{ 48, Typeface::ColorEmoji };
-	font.addFallback(emojiFont);
-
-	// ボタンを押した回数 | Number of button presses
-	int32 count = 0;
-
-	// チェックボックスの状態 | Checkbox state
-	bool checked = false;
-
-	// プレイヤーの移動スピード | Player's movement speed
-	double speed = 200.0;
-
-	// プレイヤーの X 座標 | Player's X position
-	double playerPosX = 400;
-
-	// プレイヤーが右を向いているか | Whether player is facing right
-	bool isPlayerFacingRight = true;
+	for (int y = 0; y < YCOUNT; ++y) {
+		for (int x = 0; x < XCOUNT; ++x) {
+			int index = y * XCOUNT + x;
+			bricks[index] = Rect{ x * (BRICKSIZE.x + BALLDUR), y * (BRICKSIZE.y + BALLDUR), BRICKSIZE };
+		}
+	}
 
 	while (System::Update())
 	{
-		// テクスチャを描く | Draw the texture
-		texture.draw(20, 20);
+#pragma region 更新
+		const Rect paddle{ Arg::center(Cursor::Pos().x, 500), 60, 10 };
+		ball.moveBy(ballVelocity * Scene::DeltaTime());
 
-		// テキストを描く | Draw text
-		font(U"Hello, Siv3D!🎮").draw(64, Vec2{ 20, 340 }, ColorF{ 0.2, 0.4, 0.8 });
+#pragma endregion
+#pragma region 物理
 
-		// 指定した範囲内にテキストを描く | Draw text within a specified area
-		font(U"Siv3D (シブスリーディー) は、ゲームやアプリを楽しく簡単な C++ コードで開発できるフレームワークです。")
-			.draw(18, Rect{ 20, 430, 480, 200 }, Palette::Black);
 
-		// 長方形を描く | Draw a rectangle
-		Rect{ 540, 20, 80, 80 }.draw();
+		/*if (ball.intersects(paddle)) {
+			ballVelocity.y = -BALLSPEED;
+		}*/
 
-		// 角丸長方形を描く | Draw a rounded rectangle
-		RoundRect{ 680, 20, 80, 200, 20 }.draw(ColorF{ 0.0, 0.4, 0.6 });
+		for (int i = 0; i < MAX; ++i) {
+			auto& refBrick = bricks[i];
 
-		// 円を描く | Draw a circle
-		Circle{ 580, 180, 40 }.draw(Palette::Seagreen);
+			if (refBrick.intersects(ball)) {
 
-		// 矢印を描く | Draw an arrow
-		Line{ 540, 330, 760, 260 }.drawArrow(8, SizeF{ 20, 20 }, ColorF{ 0.4 });
-
-		// 半透明の円を描く | Draw a semi-transparent circle
-		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1.0, 0.0, 0.0, 0.5 });
-
-		// ボタン | Button
-		if (SimpleGUI::Button(U"count: {}"_fmt(count), Vec2{ 520, 370 }, 120, (checked == false)))
-		{
-			// カウントを増やす | Increase the count
-			++count;
+				// ブロックの上辺、または底辺と交差
+				if (refBrick.bottom().intersects(ball) || refBrick.top().intersects(ball))
+				{
+					ballVelocity.y *= -1;
+				}
+				else
+				{
+					ballVelocity.x *= -1;
+				}
+				refBrick.y -= 600;
+				Score += 1;
+				break;
+			}
 		}
 
-		// チェックボックス | Checkbox
-		SimpleGUI::CheckBox(checked, U"Lock \U000F033E", Vec2{ 660, 370 }, 120);
-
-		// スライダー | Slider
-		SimpleGUI::Slider(U"speed: {:.1f}"_fmt(speed), speed, 100, 400, Vec2{ 520, 420 }, 140, 120);
-
-		// 左キーが押されていたら | If left key is pressed
-		if (KeyLeft.pressed())
+		// 天井との衝突を検知
+		if ((ball.y < 0) && (ballVelocity.y < 0))
 		{
-			// プレイヤーが左に移動する | Player moves left
-			playerPosX = Max((playerPosX - speed * Scene::DeltaTime()), 60.0);
-			isPlayerFacingRight = false;
+			ballVelocity.y *= -1;
 		}
 
-		// 右キーが押されていたら | If right key is pressed
-		if (KeyRight.pressed())
+		if (((ball.x < 0) && (ballVelocity.x < 0)) || ((Scene::Width() < ball.x) && (0 < ballVelocity.x)))
 		{
-			// プレイヤーが右に移動する | Player moves right
-			playerPosX = Min((playerPosX + speed * Scene::DeltaTime()), 740.0);
-			isPlayerFacingRight = true;
+			ballVelocity.x *= -1;
 		}
 
-		// プレイヤーを描く | Draw the player
-		emoji.scaled(0.75).mirrored(isPlayerFacingRight).drawAt(playerPosX, 540);
+		if ((0 < ballVelocity.y) && paddle.intersects(ball))
+		{
+			ballVelocity = Vec2{ (ball.x - paddle.center().x) * 10,-ballVelocity.y
+			}.setLength(BALLSPEED);
+		}
+
+		if (ball.y > 600)
+		{
+			isGameOver = true;
+			break;
+		}
+
+
+#pragma endregion
+#pragma region 描画
+		for (size_t i = 0; i < MAX; i++)
+		{
+			bricks[i].stretched(1).draw(HSV(bricks[i].y - 40));
+		}
+
+		paddle.rounded(3).draw();
+		ball.draw();
+
+		font(U"Score:", Score).drawAt(Scene::Center().movedBy(0, 200));
+#pragma endregion
 	}
+#pragma endregion
+
+#pragma region ゲームオーバー
+
+
+	while (System::Update())
+	{
+		font(U"Game Over").drawAt(Scene::Center(), Palette::Black);
+
+		if (SimpleGUI::ButtonAt(U"リトライ", TITLE_POS + Vec2{ 0, 100 }))
+		{
+			isGameOver = false;
+			goto title;
+		}
+	}
+#pragma endregion
+
 }
 
-//
-// - Debug ビルド: プログラムの最適化を減らす代わりに、エラーやクラッシュ時に詳細な情報を得られます。
-//
-// - Release ビルド: 最大限の最適化でビルドします。
-//
-// - [デバッグ] メニュー → [デバッグの開始] でプログラムを実行すると、[出力] ウィンドウに詳細なログが表示され、エラーの原因を探せます。
-//
-// - Visual Studio を更新した直後は、プログラムのリビルド（[ビルド]メニュー → [ソリューションのリビルド]）が必要な場合があります。
-//
+class State
+{
+	void update() {}
+};
